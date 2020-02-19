f4dcb1d333bf4dc8223b05a614f2a08433d7d5dd
danbulant <danbulant@gmail.com>
2020-02-19 17:45:56 +0100

    Get info of given package

--- modules/package.js
+++ modules/package.js
@@ -1,8 +1,11 @@
 const fs = require("fs");
+const request = require("./requests");
+
+const REPO = "https://registry.npmjs.org/";
 
 module.exports = class Package {
     pkg = {};
-    
+
     load(path){
         if(!fs.existsSync(path))throw Error("Path doesn't exists!");
 
@@ -15,5 +18,24 @@ module.exports = class Package {
         this.pkg = json;
 
         return json;
+    }
+
+    get(pkg){
+        return new Promise((res, rej) => {
+            if(!pkg)return rej("No package name given");
+
+            request(REPO + pkg + "/").then((res) => {
+                var o = JSON.parse(res);
+                if(o.error == "Not found"){
+                    console.log("The package providen couldn't be found on the NPM repository");
+                    return rej(404);
+                }
+
+                res("found");
+            }).catch(e => {
+                console.warn(e);
+                rej(e);
+            })
+        });
     }
 }
