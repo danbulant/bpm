cb34fb473b4666d0188919eda6c6507bf9033cb6
danbulant <danbulant@gmail.com>
2020-02-21 17:04:19 +0100

    Test `bpm install chalk`

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 const got = require("got");
+const chalk = require("chalk");
 
 console.log("Trying to make request...");
 got("http://google.com").then((r) => {
-    console.log("Got request working!");
+    console.log(chalk.blue("Got request working!"));
 })
