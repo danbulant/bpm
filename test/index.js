17dc0ac86d1e4bff50320b5edc82cb52da5a040a
danbulant <danbulant@gmail.com>
2020-02-21 20:10:30 +0100

    Test error exit

--- test/index.js
+++ test/index.js
@@ -4,4 +4,7 @@ const chalk = require("chalk");
 console.log("Trying to make request...");
 got("http://google.com").then((r) => {
     console.log(chalk.blue("Got request working!"));
-})
+    
+    console.error(chalk.red("Exiting with code 1 to test error cases"));
+    process.exit(1);
+});
